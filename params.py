493b56fff3dbd5f9c5f3cd66f537d445c46452a6
Mathieu Kretz <kretz.mathieu@gmail.com>
2023-11-28 11:52:24 +0100

    colonnes retenues modifiées vite fait

--- params.py
+++ params.py
@@ -1,6 +1,6 @@
 #colonnes intéressantes que l'on garde
-USEFUL_FEATS = ['rating', 'channels', 'species',  'sampling_rate' , 'file_type']
+USEFUL_FEATS = ['filename','species', 'rating', 'channels', 'sampling_rate' , 'file_type']
 
 #colonnes certainement inutiles
-MAYBE_FEATS = ['playback_used','bitrate_of_mp3', 'ebird_code', 'pitch', 'filename', 'speed', 'number_of_notes', 'title', 'bird_seen',
+MAYBE_FEATS = ['playback_used','bitrate_of_mp3', 'ebird_code', 'pitch', 'speed', 'number_of_notes', 'title', 'bird_seen',
                 'sci_name', 'type', 'description', 'background', 'xc_id', 'primary_label']
