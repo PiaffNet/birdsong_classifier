d8a0a2cc6c1e9a125c0c9ab8abbb6a618c01e24b
Mathieu Kretz <kretz.mathieu@gmail.com>
2023-11-28 10:49:02 +0100

    j'ai restreint à 5 les colonnes qu'on garde

--- params.py
+++ params.py
@@ -1,1 +1,6 @@
-USEFUL_FEATS = ['rating', 'playback_used', 'channels', 'species',  'sampling_rate', 'bitrate_of_mp3', 'file_type']
+#colonnes intéressantes que l'on garde
+USEFUL_FEATS = ['rating', 'channels', 'species',  'sampling_rate' , 'file_type']
+
+#colonnes certainement inutiles
+MAYBE_FEATS = ['playback_used','bitrate_of_mp3', 'ebird_code', 'pitch', 'filename', 'speed', 'number_of_notes', 'title', 'bird_seen',
+                'sci_name', 'type', 'description', 'background', 'xc_id', 'primary_label']
