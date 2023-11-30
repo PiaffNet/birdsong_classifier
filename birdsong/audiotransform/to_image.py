0c93be1008c31c5aaad9694eb6d8c71e16d6d024
Raman Sheshka <raman.sheshka@gmail.com>
2023-11-30 16:44:40 +0100

    clean dumb functions

--- birdsong/audiotransform/to_image.py
+++ birdsong/audiotransform/to_image.py
@@ -16,43 +16,6 @@ def select_species_per_country(df, country="France"):
     df_clean = (df[df["species"].isin(species_fr)])[USEFUL_FEATS]
     return df_clean
 
-
-"""
-Convert audio file to single channel (mono) and standard sample rate (48k)
-#USEFUL_FEATS = ['filename','species', 'rating', 'channels', 'sampling_rate' , 'file_type']
-
-
-"""
-#Garde seulement les espèces françaises et les colonnes intéressantes. Cela fait
-#2706 lignes et 7 colonnes. Il reste 257 na dans le playback_used, et 2 dans le bitrate
-#"""
-#def clean_data(df):
-#    species_fr = df[df["country"]== "France"]["species"].unique()
-#    df_clean = (df[df["species"].isin(species_fr)])[USEFUL_FEATS]
-#    return df_clean
-
-
-
-# def sound_to_image(filename):
-
-#     #load the file
-#     y = librosa.load(filename)
-#     # Compute the mel-spectrogram
-#     sample_rate = 48000 #window size of 10.7 ms (512 samples at 48 kHz)
-#     n_fft = 512
-#     f_min = 150 #frequency range between 150 et 15 kHz
-#     f_max = 15000
-#     hop_length = round(0.75*n_fft) # 25 % de overlap, donc 0.75 * n_fft
-#     n_mels = 64 #mel scale with 64 bands
-#     htk = 1750 #break frequency
-
-#     mel_spectrogram = librosa.feature.melspectrogram(y = y , sr=sample_rate, n_fft = n_fft, n_mels = n_mels, htk= htk, hop_length= hop_length, fmin = f_min, fmax= f_max)
-#     mel_spectrogram_db = librosa.power_to_db(mel_spectrogram)
-
-#     return mel_spectrogram_db
-
-
-
 """ Convert audio file to single channel (mono) and standard sample rate (48k)
     --> takes input folder containing raw audio files and creates new output folder
     --> specifies target sample rate (48k Hz) and nb of channels (mono by default)
@@ -82,9 +45,6 @@ class AudioPreprocessor:
             for file_path in  file_path_list:
                 print(f"prepare processing of {file_path}")
                 self.preprocess_audio(file_path, target_directory)
-
-    def load_audio_signal(file, sr):
-        return librosa.load(file, mono=True, sr=config.SAMPLING_RATE)
 
     def preprocess_audio(self, file_path, target_directory):
         #input_path = os.path.join(self.input_folder, file_name)
