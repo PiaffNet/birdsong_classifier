537b5b33636237831bf66bf8728e37b38b2ceb1e
Raman Sheshka <raman.sheshka@gmail.com>
2023-12-02 05:15:08 +0100

    import correction

--- birdsong/model/model_collections.py
+++ birdsong/model/model_collections.py
@@ -56,26 +56,26 @@ Modèles basés sur BirdNet
 Definition des blocks de downsampling et de ResBlock
 """
 def resblock(x, kernelsize, filters):
-    fx = layers.Conv2D(filters, kernelsize, activation='relu', padding='same')(x)
-    fx = layers.BatchNormalization()(fx)
-    fx = layers.Conv2D(filters, kernelsize, padding='same')(fx)
-    out = layers.Add()([x,fx])
-    out = layers.ReLU()(out)
-    out = layers.BatchNormalization()(out)
+    fx = keras.layers.Conv2D(filters, kernelsize, activation='relu', padding='same')(x)
+    fx = keras.layers.BatchNormalization()(fx)
+    fx = keras.layers.Conv2D(filters, kernelsize, padding='same')(fx)
+    out = keras.layers.Add()([x,fx])
+    out = keras.layers.ReLU()(out)
+    out = keras.layers.BatchNormalization()(out)
     return out
 
 def downsampling(x, filters):
   ### Branche 1
-  b1= layers.Conv2D(filters, kernel_size = 1, activation = 'relu') (x)
-  b1 = layers.Conv2D(filters, kernel_size = 3, strides = 2, padding = 'same',activation = 'relu') (b1)
-  b1= layers.Conv2D(filters, kernel_size = 1, activation = 'relu') (b1)
+  b1= keras.layers.Conv2D(filters, kernel_size = 1, activation = 'relu') (x)
+  b1 = keras.layers.Conv2D(filters, kernel_size = 3, strides = 2, padding = 'same',activation = 'relu') (b1)
+  b1= keras.layers.Conv2D(filters, kernel_size = 1, activation = 'relu') (b1)
 
   ### Branche 2
-  b2 = layers.AveragePooling2D(pool_size = (2,2), strides = 2, padding = 'same') (x)
-  b2= layers.Conv2D(filters, kernel_size = 1, activation = 'relu') (b2)
+  b2 = keras.layers.AveragePooling2D(pool_size = (2,2), strides = 2, padding = 'same') (x)
+  b2= keras.layers.Conv2D(filters, kernel_size = 1, activation = 'relu') (b2)
 
-  out = layers.Add()([b1,b2])
-  out = layers.ReLU()(out)
+  out = keras.layers.Add()([b1,b2])
+  out = keras.layers.ReLU()(out)
   return out
 
 
@@ -84,13 +84,13 @@ def downsampling(x, filters):
 Modèle simplifié, avec une seule layer de ResStack
 """
 
-def build_archi_2(num_classes: int, input_shape=(64,376,1)) -> Model:
+def build_archi_2(num_classes: int, input_shape=(64,376,1)) -> keras.Model:
         #preprocessing
-    visible = Input(shape=input_shape)
-    conv1 = layers.Conv2D(32, kernel_size=5, padding ='same', activation='relu')(visible)
-    bn1 = layers.BatchNormalization() (conv1)
-    relu1 = layers.ReLU() (bn1)
-    pool1 = layers.MaxPooling2D(pool_size=(1, 2))(relu1)
+    visible = keras.Input(shape=input_shape)
+    conv1 = keras.layers.Conv2D(32, kernel_size=5, padding ='same', activation='relu')(visible)
+    bn1 = keras.layers.BatchNormalization() (conv1)
+    relu1 = keras.layers.ReLU() (bn1)
+    pool1 = keras.layers.MaxPooling2D(pool_size=(1, 2))(relu1)
 
     #ResStack1
 
@@ -99,24 +99,24 @@ def build_archi_2(num_classes: int, input_shape=(64,376,1)) -> Model:
 
     # Classification
 
-    conv5= layers.Conv2D(64, kernel_size = 3, strides = 4, padding = 'same', activation = 'relu') (res1)
-    bn5 = layers.BatchNormalization() (conv5)
-    relu5 = layers.ReLU() (bn5)
-    drop5 = layers.Dropout(rate=0.2) (relu5)
+    conv5= keras.layers.Conv2D(64, kernel_size = 3, strides = 4, padding = 'same', activation = 'relu') (res1)
+    bn5 = keras.layers.BatchNormalization() (conv5)
+    relu5 = keras.layers.ReLU() (bn5)
+    drop5 = keras.layers.Dropout(rate=0.2) (relu5)
 
-    conv6= layers.Conv2D(32, kernel_size = 3, padding = 'same', activation = 'relu') (drop5)
-    bn6 = layers.BatchNormalization() (conv6)
-    relu6 = layers.ReLU() (bn6)
-    drop6 = layers.Dropout(rate=0.26) (relu6)
+    conv6= keras.layers.Conv2D(32, kernel_size = 3, padding = 'same', activation = 'relu') (drop5)
+    bn6 = keras.layers.BatchNormalization() (conv6)
+    relu6 = keras.layers.ReLU() (bn6)
+    drop6 = keras.layers.Dropout(rate=0.26) (relu6)
 
-    conv7 = layers.Conv2D(16, kernel_size = 3, padding = 'same', activation = 'relu') (drop6)
-    bn7 = layers.BatchNormalization() (conv7)
+    conv7 = keras.layers.Conv2D(16, kernel_size = 3, padding = 'same', activation = 'relu') (drop6)
+    bn7 = keras.layers.BatchNormalization() (conv7)
 
-    flat = Flatten()(bn7)
-    dense1 = layers.Dense(256, activation='relu')(flat)
+    flat = keras.Flatten()(bn7)
+    dense1 = keras.layers.Dense(256, activation='relu')(flat)
 
-    output = Dense(num_classes, activation='softmax')(dense1)
-    model = Model(inputs=visible, outputs=output)
+    output = keras.Dense(num_classes, activation='softmax')(dense1)
+    model = keras.Model(inputs=visible, outputs=output)
 
     return model
 
@@ -124,13 +124,13 @@ def build_archi_2(num_classes: int, input_shape=(64,376,1)) -> Model:
 """"
 Modèle fini, plus long à computer
 """
-def build_archi_3(num_classes: int, input_shape=(64,376,1)) -> Model:
+def build_archi_3(num_classes: int, input_shape=(64,376,1)) -> keras.Model:
     #preprocessing
     visible = Input(shape=input_shape)
-    conv1 = layers.Conv2D(32, kernel_size=5, padding ='same', activation='relu')(visible)
-    bn1 = layers.BatchNormalization() (conv1)
-    relu1 = layers.ReLU() (bn1)
-    pool1 = layers.MaxPooling2D(pool_size=(1, 2))(relu1)
+    conv1 = keras.layers.Conv2D(32, kernel_size=5, padding ='same', activation='relu')(visible)
+    bn1 = keras.layers.BatchNormalization() (conv1)
+    relu1 = keras.layers.ReLU() (bn1)
+    pool1 = keras.layers.MaxPooling2D(pool_size=(1, 2))(relu1)
 
     #ResStack1
 
@@ -153,23 +153,23 @@ def build_archi_3(num_classes: int, input_shape=(64,376,1)) -> Model:
     res4 = resblock(ds4, 2, 512)
 
     # Classification
-    conv5= layers.Conv2D(512, kernel_size = 3, strides = 4, padding = 'same', activation = 'relu') (res4)
-    bn5 = layers.BatchNormalization() (conv5)
-    relu5 = layers.ReLU() (bn5)
-    drop5 = layers.Dropout(rate=0.24) (relu5)
+    conv5= keras.layers.Conv2D(512, kernel_size = 3, strides = 4, padding = 'same', activation = 'relu') (res4)
+    bn5 = keras.layers.BatchNormalization() (conv5)
+    relu5 = keras.layers.ReLU() (bn5)
+    drop5 = keras.layers.Dropout(rate=0.24) (relu5)
 
-    conv6= layers.Conv2D(256, kernel_size = 3, padding = 'same', activation = 'relu') (drop5)
-    bn6 = layers.BatchNormalization() (conv6)
-    relu6 = layers.ReLU() (bn6)
-    drop6 = layers.Dropout(rate=0.26) (relu6)
+    conv6= keras.layers.Conv2D(256, kernel_size = 3, padding = 'same', activation = 'relu') (drop5)
+    bn6 = keras.layers.BatchNormalization() (conv6)
+    relu6 = keras.layers.ReLU() (bn6)
+    drop6 = keras.layers.Dropout(rate=0.26) (relu6)
 
-    conv7 = layers.Conv2D(128, kernel_size = 3, padding = 'same', activation = 'relu') (drop6)
-    bn7 = layers.BatchNormalization() (conv7)
-    drop7 = layers.Dropout(rate=0.3) (bn7)
+    conv7 = keras.layers.Conv2D(128, kernel_size = 3, padding = 'same', activation = 'relu') (drop6)
+    bn7 = keras.layers.BatchNormalization() (conv7)
+    drop7 = keras.layers.Dropout(rate=0.3) (bn7)
 
-    flat = Flatten()(bn7)
-    output = Dense(num_classes, activation='softmax')(flat)
-    model = Model(inputs=visible, outputs=output)
+    flat = keras.Flatten()(bn7)
+    output = keras.Dense(num_classes, activation='softmax')(flat)
+    model = keras.Model(inputs=visible, outputs=output)
 
     return model
 
